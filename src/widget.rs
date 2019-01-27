10e494312fa13013833423c16eb7f30012a4fa7d
Brent Gardner <bgardner@squarelabs.net>
2019-01-27 16:39:48 -0700

    Add a grid

--- src/widget.rs
+++ src/widget.rs
@@ -1,6 +1,6 @@
 use piston_window::{G2d, Context};
 
 pub trait Widget {
-    fn draw(&self, context: Context, gl: &mut G2d, width: u32, height: u32);
+    fn draw(&self, context: Context, gl: &mut G2d, width: f64, height: f64);
 }
 
