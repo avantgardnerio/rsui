ab7d4e52b5313679d15113daed6d1533b3964f7c
Brent Gardner <bgardner@squarelabs.net>
2019-01-27 17:02:40 -0700

    Grid (#3)
    * Upgrade to latest
    
    * Add a grid

--- src/grid_panel.rs
+++ src/grid_panel.rs
@@ -1,4 +1,4 @@
-use piston_window::{G2d, Context, Rectangle, rectangle};
+use piston_window::{G2d, Context, Rectangle, rectangle, line};
 
 use crate::widget::Widget;
 
@@ -15,9 +15,17 @@ impl GridPanel {
 }
 
 impl Widget for GridPanel {
-    fn draw(&self, context: Context, gl: &mut G2d, width: u32, height: u32) {
+    fn draw(&self, context: Context, gl: &mut G2d, width: f64, height: f64) {
         let rectangle = Rectangle::new(self.background_color);
         let square = rectangle::rectangle_by_corners(0.0, 0.0, width as f64, height as f64);
-        rectangle.draw(square, &context.draw_state, context.transform, gl);
+        rectangle.draw(square, &context.draw_state, context.transform.clone(), gl);
+
+        let white = [1.0, 1.0, 1.0, 1.0];
+        for x in (0..(width as i32)).step_by(100) {
+            line(white, 1.0, [x as f64, 0.0, x as f64, height], context.transform.clone(), gl);
+        }
+        for y in (0..(height as i32)).step_by(100) {
+            line(white, 1.0, [0.0, y as f64, width, y as f64], context.transform.clone(), gl);
+        }
     }
 }
