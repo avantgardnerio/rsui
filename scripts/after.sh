c8120e264882c036e085d6a9af29353d9332ee8c
Brent Gardner <bgardner@squarelabs.net>
2019-01-28 07:52:46 -0700

    test

--- scripts/after.sh
+++ scripts/after.sh
@@ -1,7 +1,42 @@
 #!/usr/bin/env bash
-git tag $NEW_VER
-git push --tags
 
-cargo login $CRATESIO_TOKEN
-cargo package --allow-dirty
-cargo publish --allow-dirty
+BRANCH=$(git branch | grep -o master)
+echo "Branch=$BRANCH"
+if [ "$BRANCH" = "master" ]
+  then
+    echo "On master, performing release..."
+
+    # determine OS
+    UNAME="$(uname -s)"
+    case "${UNAME}" in
+        Linux*)     DIST=linux ZIP_EXT=tgz;;
+        Darwin*)    DIST=darwin ZIP_EXT=tgz;;
+        CYGWIN*)    DIST=windows ZIP_EXT=zip;;
+        MINGW*)     DIST=windows ZIP_EXT=zip;;
+        *)          DIST="UNKNOWN:${UNAME}"
+    esac
+    echo DIST=${DIST} EXT=$ZIP_EXT
+
+    # github cli
+    HUB_VER=2.8.3
+    cd /tmp
+    wget -q https://github.com/github/hub/releases/download/v$HUB_VER/hub-$DIST-amd64-$HUB_VER.$ZIP_EXT
+    if [ "$ZIP_EXT" == "tgz" ]
+    then
+        tar -xf hub-$DIST-amd64-$HUB_VER.$ZIP_EXT
+    else
+        unzip hub-$DIST-amd64-$HUB_VER.$ZIP_EXT
+    fi
+    cd -
+    mv /tmp/hub*/bin/hub .
+    hub version
+
+    # tag in git
+    git tag $NEW_VER
+    git push --tags
+
+    # publish crate
+    cargo login $CRATESIO_TOKEN
+    cargo package --allow-dirty
+    cargo publish --allow-dirty
+fi
--- scripts/before.sh
+++ scripts/before.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-printenv
-which hub
-hub version
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
+set -x
+
+BRANCH=$(git branch | grep -o master)
 echo "Branch=$BRANCH"
 if [ "$BRANCH" = "master" ]
   then
-    echo "On master, performing release..."
+    echo "On master, updating cargo version..."
+
     PREV_VER=$(git describe --tags HEAD~1 | awk -F'[-.]' '{print $1"."$2"."$3}')
     echo "Previous version was $PREV_VER..."
     NEW_VER=$(echo $PREV_VER | awk -F'[-.]' '{print $1"."$2+1"."$3}')
     echo "Upgrading to $NEW_VER..."
     sed -i '' "s/version = \"[0-9\.]*\"/version = \"$NEW_VER\"/g" Cargo.toml
+    cat Cargo.toml
 fi
