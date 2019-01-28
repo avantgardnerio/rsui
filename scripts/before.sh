814f0b00a11ab1b5bffde6d7e67635e1b90e3eb2
Brent Gardner <bgardner@squarelabs.net>
2019-01-28 06:45:17 -0700

    test

--- scripts/before.sh
+++ scripts/before.sh
@@ -3,11 +3,11 @@ BRANCH=$(git rev-parse --abbrev-ref HEAD)
 if [ "$BRANCH" = "master" ]
   then
     echo "On master, performing release..."
-    PREV_VER=$(git describe --tags HEAD~1)
+    PREV_VER=$(git describe --tags HEAD~1 | awk -F'[-.]' '{print $1"."$2"."$3}')
     echo "Previous version was $PREV_VER..."
     NEW_VER=$(echo $PREV_VER | awk -F'[-.]' '{print $1"."$2+1"."$3}')
     echo "Upgrading to $NEW_VER..."
-    #sed -i '' "s/version = \"[0-9\.]*\"/version = \"$NEW_VER\"/g" Cargo.toml
+    sed -i '' "s/version = \"[0-9\.]*\"/version = \"$NEW_VER\"/g" Cargo.toml
     git tag $NEW_VER
     git push --tags
 fi
