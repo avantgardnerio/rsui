ba2973ab40aeacf5b96a4ec586f887e8a440f939
Brent Gardner <bgardner@squarelabs.net>
2019-01-28 07:57:00 -0700

    test

--- scripts/before.sh
+++ scripts/before.sh
@@ -11,6 +11,6 @@ if [ "$BRANCH" = "master" ]
     echo "Previous version was $PREV_VER..."
     NEW_VER=$(echo $PREV_VER | awk -F'[-.]' '{print $1"."$2+1"."$3}')
     echo "Upgrading to $NEW_VER..."
-    sed -i '' "s/version = \"[0-9\.]*\"/version = \"$NEW_VER\"/g" Cargo.toml
+    sed -i.bak "s/version = \"[0-9\.]*\"/version = \"$NEW_VER\"/g" Cargo.toml
     cat Cargo.toml
 fi
