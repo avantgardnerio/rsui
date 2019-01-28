8325756ed989eadcd38853968a75ad9c6650d8a3
Brent Gardner <bgardner@squarelabs.net>
2019-01-28 07:14:10 -0700

    test

--- scripts/after.sh
+++ scripts/after.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+git tag $NEW_VER
+git push --tags
+
 cargo login $CRATESIO_TOKEN
 cargo package --allow-dirty
 cargo publish --allow-dirty
--- scripts/before.sh
+++ scripts/before.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 printenv
+which hub
+hub version
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo "Branch=$BRANCH"
 if [ "$BRANCH" = "master" ]
@@ -10,6 +12,4 @@ if [ "$BRANCH" = "master" ]
     NEW_VER=$(echo $PREV_VER | awk -F'[-.]' '{print $1"."$2+1"."$3}')
     echo "Upgrading to $NEW_VER..."
     sed -i '' "s/version = \"[0-9\.]*\"/version = \"$NEW_VER\"/g" Cargo.toml
-    git tag $NEW_VER
-    git push --tags
 fi
