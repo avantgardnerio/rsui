d88a4b1c7fdcc1b8fbb0e7b50fff8c089c366258
Brent Gardner <bgardner@squarelabs.net>
2019-01-28 05:54:16 -0700

    Tagging

--- release.sh
+++ release.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
-NEXT_VER=$(git describe --tags | awk -F'[-.]' '{print $1"."$2+1"."$3}')
-echo "Upgrading to $NEXT_VER..."
-sed -i '' "s/version = \"[0-9\.]*\"/version = \"$NEXT_VER\"/g" Cargo.toml
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ "$BRANCH" = "master" ]
+  then
+    echo "On master, performing release..."
+    PREV_VER=$(git describe --tags HEAD~1)
+    echo "Previous version was $PREV_VER..."
+    NEW_VER=$(echo $PREV_VER | awk -F'[-.]' '{print $1"."$2+1"."$3}')
+    echo "Upgrading to $NEW_VER..."
+    #sed -i '' "s/version = \"[0-9\.]*\"/version = \"$NEW_VER\"/g" Cargo.toml
+    git tag $NEW_VER
+    git push --tags
+fi
